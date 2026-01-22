d66b0c009c248de7a8d1cdd33b22178c0ac67d62
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-01-22 10:17:35 +0545

    made the player stop from going out of the boundary and also tried to make a program to aatach the position of player to the bullet

--- my-first-game-/player/player.gd
+++ my-first-game-/player/player.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var direction:int = 0
-var speed:int =400
+var speed:int =500
+var bullet_fire = preload("res://fires_red.tscn")
+var shoot_time:=1
+var shoot_count=0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +14,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed("shoot") and shoot_count>shoot_time:
+		
+		
 	if Input.is_action_pressed("move_element_left"):
 		direction=-1
 	elif Input.is_action_pressed("move_element_right"):
@@ -18,5 +24,14 @@ func _process(delta):
 	else:
 		direction =0
 	position.x+= direction*delta*speed
+	
+	#for protecting the player from going outside the boundary
+	var width_scr = get_viewport_rect().size.x
+	 #in my words the ed 
+	#clamp commands stop the person from going beyond the screen
+	#and the position.x is position of the player and
+	#0 is the inital point i.e (left wall cause it is like a origin 
+	#the width_scr is the position of the right wall that we declar
+	position.x = clamp(position.x,0,width_scr)
 	
 	
